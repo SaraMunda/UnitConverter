342cc10a996ea602e70f46b170f2954926e841e6
Sara <saramunda99@gmail.com>
2023-10-16 11:47:40 +0200

    I made the conversion and display the result

--- index.js
+++ index.js
@@ -4,23 +4,86 @@ const result = document.getElementById("result")
 const fromUnit = document.getElementById ("fromUnit")
 const toUnit = document.getElementById("toUnit")
 
+//update unit option
 
-const options = {
-    length: ["Meter", "Centimeter", "Kilometer", "Millimeter", "Foot", "Inch"],
-    weight: ["Gram", "Milligram", "Kilogram", "Ounce", "Pound"],
-    time: ["Second", "Minute", "Hour", "Day", "Week", "Month", "Year"],
-    temperature: ["Kelvin", "Celsius", "Fahrnheit"]
+const conversionRates = {
+  length: {
+      Meter: 1,
+      Centimeter: 100,
+      Kilometer: 0.001,
+      Millimeter: 1000,
+      Foot: 3.28084,
+      Inch: 39.3701
+  },
+  weight: {
+      Gram: 1,
+      Milligram: 1000,
+      Kilogram: 0.001,
+      Ounce: 0.03527396,
+      Pound: 0.00220462
+  },
+  time: {
+      Second: 1,
+      Minute: 1 / 60,
+      Hour: 1 / 3600,
+      Day: 1 / 86400,
+      Week: 1 / 604800,
+      Month: 1 / 2628000,
+      Year: 1 / 31536000
+  },
+  temperature: {
+      Kelvin: {
+          celsius: kelvin => kelvin - 273.15,
+          fahrenheit: kelvin => (kelvin - 273.15) * 9/5 + 32
+      },
+      Celsius: {
+          kelvin: celsius => celsius + 273.15,
+          fahrenheit: celsius => celsius * 9/5 + 32
+      },
+      Fahrenheit: {
+          kelvin: fahrenheit => (fahrenheit - 32) * 5/9 + 273.15,
+          celsius: fahrenheit => (fahrenheit - 32) * 5/9
+      }
   }
-  
-  function updateUnitOptions(selectElement) {
-    const selectedValue = property.value
-    selectElement.innerHTML = options[selectedValue].map(option => `<option>${option}</option>`).join('')
-  }
-  
+}
+
+function updateUnitOptions(selectElement) {
+  const selectedValue = property.value
+  selectElement.innerHTML = Object.keys(conversionRates[selectedValue]).map(unit => `<option>${unit}</option>`).join('')
+}
+
+updateUnitOptions(fromUnit)
+updateUnitOptions(toUnit)
+
+property.addEventListener("change", function() {
   updateUnitOptions(fromUnit)
   updateUnitOptions(toUnit)
-  
-  property.addEventListener("change", function () {
-    updateUnitOptions(fromUnit)
-    updateUnitOptions(toUnit)
-  })
+})
+
+//display the result
+
+const finalResult = () => {
+  const inputValue = parseFloat(input.value)
+
+  if (!isNaN(inputValue)) {
+      const selectedProperty = property.value
+      const fromUnitValue = fromUnit.value
+      const toUnitValue = toUnit.value
+
+      if (conversionRates[selectedProperty] && conversionRates[selectedProperty][fromUnitValue] && conversionRates[selectedProperty][toUnitValue]) {
+          const resultValue = inputValue * (conversionRates[selectedProperty][toUnitValue] / conversionRates[selectedProperty][fromUnitValue])
+          result.value = resultValue
+      }
+  }
+}
+
+input.addEventListener("input", function() {
+  if (input.value === "") {
+      result.value = ""  
+  } else {
+    finalResult()
+  }
+})
+
+fromUnit.addEventListener("change", finalResult)
+toUnit.addEventListener("change", finalResult)
